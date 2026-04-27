05b7910a760e9b8a74c00c2c6301e3e62ef27013
Luis Alvarado Aguirre <luisalvaradoaguirre@MacBook-Air-de-Luis.local>
2026-04-28 00:05:55 +0200

    Fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'jest';
 
 const config: Config = {
   testEnvironment: 'jsdom',
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
